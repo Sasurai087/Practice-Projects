9343417eb99f604694503086abeeab1e278cd50e
James Ohsako <johsako087@gmail.com>
2021-08-13 13:56:06 -0700

    scaffold app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseItem = ({ props }) => {
+  return (
+    <div>
+      <div>Aug 11, 2021</div>
+      <div>
+        <h2>Monster Hunter Rise</h2>
+        <div>$43.00</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
